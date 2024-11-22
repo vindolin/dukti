7a7fffc01fde6b419f9a79f87e437df63a7d022a
Thomas Schüßler <834347+vindolin@users.noreply.github.com>
2024-11-22 16:24:30 +0100

    refactor main.dart to use hooks and remove AppLifecycleManager; invalidate client list on app resume

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:dukti/services/clipboard_service.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter/material.dart';
 
 import '/screens/home_screen.dart';
 import '/services/bonjour_service.dart' as bonsoir;
 import '/services/server_port_service.dart';
 import '/models/client_name.dart';
+import '/models/client_provider.dart';
 import '/services/webserver_service.dart';
+import '/platform_helper.dart';
 
 import '/logger.dart';
 
@@ -16,44 +20,6 @@ import '/logger.dart';
   and the old device is still being broadcasted.
   see: https://stackoverflow.com/questions/69952232/handling-dispose-method-when-using-hot-restart
 */
-
-class AppLifecycleManager extends StatefulWidget {
-  final Widget child;
-
-  const AppLifecycleManager({super.key, required this.child});
-
-  @override
-  AppLifecycleManagerState createState() => AppLifecycleManagerState();
-}
-
-class AppLifecycleManagerState extends State<AppLifecycleManager> with WidgetsBindingObserver {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-  }
-
-  @override
-  void dispose() {
-    // Perform cleanup here
-    // For example, stop servers, close streams, etc.
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.detached) {
-      // The app is exiting
-      // Perform cleanup if needed
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return widget.child;
-  }
-}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -69,13 +35,29 @@ void main() async {
   );
 }
 
-class DuktiApp extends ConsumerWidget {
+class DuktiApp extends HookConsumerWidget {
   const DuktiApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     logger.t('Building DuktiApp');
 
+    // If the app is resumed, the client list is probably outdated, invalidate it and start a new discovery.
+    final appLifecycleState = useAppLifecycleState();
+    useEffect(() {
+      if (!platformIsDesktop) {
+        if (appLifecycleState == AppLifecycleState.resumed) {
+          // without the addPostFrameCallback, the error "Cannot listen to inherited widgets inside HookState.initState. Use HookState.build instead"
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            ref.invalidate(duktiClientsProvider); // clear the client list
+            ref.invalidate(bonsoir.eventsProvider); // restart the bonsoir discovery
+          });
+        }
+      }
+      return null;
+    }, [appLifecycleState]);
+
+    // eagerly start the clipboard service, webserver and bonsoir discovery
     ref.watch(clipboardServiceProvider);
     ref.watch(startWebServerProvider(serverPort!));
     ref.watch(bonsoir.eventsProvider);
