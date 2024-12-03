3a6cd15900bed3fd60a19c0a2baf8c2a033879af
Thomas Schüßler <834347+vindolin@users.noreply.github.com>
2024-12-03 10:03:16 +0100

    refactor upload widget to use Riverpod for upload progress management

---
+++ lib/widgets/upload_widget.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'upload_widget.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$uploadProgressHash() => r'a1c9fcec5067ca63594a23b2da7cafdd17ee4865';
+
+/// See also [UploadProgress].
+@ProviderFor(UploadProgress)
+final uploadProgressProvider =
+    AutoDisposeNotifierProvider<UploadProgress, double>.internal(
+  UploadProgress.new,
+  name: r'uploadProgressProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$uploadProgressHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$UploadProgress = AutoDisposeNotifier<double>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
