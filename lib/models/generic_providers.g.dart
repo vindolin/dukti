79b65beec4f7466efd1a933a12ca0b38bb8ceb4b
Thomas Schüßler <834347+vindolin@users.noreply.github.com>
2024-12-04 10:42:43 +0100

    WIP

--- lib/models/generic_providers.g.dart
+++ lib/models/generic_providers.g.dart
@@ -6,7 +6,7 @@ part of 'generic_providers.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$togglerHash() => r'e0cf43b39f4d0e3b98726ca0c1fd5a71ed62cc82';
+String _$togglerFalseHash() => r'4b4db5274330c9818980dcd3eeb02d15fd255948';
 
 /// Copied from Dart SDK
 class _SystemHash {
@@ -29,7 +29,7 @@ class _SystemHash {
   }
 }
 
-abstract class _$Toggler extends BuildlessNotifier<bool> {
+abstract class _$TogglerFalse extends BuildlessNotifier<bool> {
   late final String key;
 
   bool build(
@@ -37,27 +37,27 @@ abstract class _$Toggler extends BuildlessNotifier<bool> {
   );
 }
 
-/// See also [Toggler].
-@ProviderFor(Toggler)
-const togglerProvider = TogglerFamily();
+/// See also [TogglerFalse].
+@ProviderFor(TogglerFalse)
+const togglerFalseProvider = TogglerFalseFamily();
 
-/// See also [Toggler].
-class TogglerFamily extends Family<bool> {
-  /// See also [Toggler].
-  const TogglerFamily();
+/// See also [TogglerFalse].
+class TogglerFalseFamily extends Family<bool> {
+  /// See also [TogglerFalse].
+  const TogglerFalseFamily();
 
-  /// See also [Toggler].
-  TogglerProvider call(
+  /// See also [TogglerFalse].
+  TogglerFalseProvider call(
     String key,
   ) {
-    return TogglerProvider(
+    return TogglerFalseProvider(
       key,
     );
   }
 
   @override
-  TogglerProvider getProviderOverride(
-    covariant TogglerProvider provider,
+  TogglerFalseProvider getProviderOverride(
+    covariant TogglerFalseProvider provider,
   ) {
     return call(
       provider.key,
@@ -76,28 +76,29 @@ class TogglerFamily extends Family<bool> {
       _allTransitiveDependencies;
 
   @override
-  String? get name => r'togglerProvider';
+  String? get name => r'togglerFalseProvider';
 }
 
-/// See also [Toggler].
-class TogglerProvider extends NotifierProviderImpl<Toggler, bool> {
-  /// See also [Toggler].
-  TogglerProvider(
+/// See also [TogglerFalse].
+class TogglerFalseProvider extends NotifierProviderImpl<TogglerFalse, bool> {
+  /// See also [TogglerFalse].
+  TogglerFalseProvider(
     String key,
   ) : this._internal(
-          () => Toggler()..key = key,
-          from: togglerProvider,
-          name: r'togglerProvider',
+          () => TogglerFalse()..key = key,
+          from: togglerFalseProvider,
+          name: r'togglerFalseProvider',
           debugGetCreateSourceHash:
               const bool.fromEnvironment('dart.vm.product')
                   ? null
-                  : _$togglerHash,
-          dependencies: TogglerFamily._dependencies,
-          allTransitiveDependencies: TogglerFamily._allTransitiveDependencies,
+                  : _$togglerFalseHash,
+          dependencies: TogglerFalseFamily._dependencies,
+          allTransitiveDependencies:
+              TogglerFalseFamily._allTransitiveDependencies,
           key: key,
         );
 
-  TogglerProvider._internal(
+  TogglerFalseProvider._internal(
     super._createNotifier, {
     required super.name,
     required super.dependencies,
@@ -111,7 +112,7 @@ class TogglerProvider extends NotifierProviderImpl<Toggler, bool> {
 
   @override
   bool runNotifierBuild(
-    covariant Toggler notifier,
+    covariant TogglerFalse notifier,
   ) {
     return notifier.build(
       key,
@@ -119,10 +120,10 @@ class TogglerProvider extends NotifierProviderImpl<Toggler, bool> {
   }
 
   @override
-  Override overrideWith(Toggler Function() create) {
+  Override overrideWith(TogglerFalse Function() create) {
     return ProviderOverride(
       origin: this,
-      override: TogglerProvider._internal(
+      override: TogglerFalseProvider._internal(
         () => create()..key = key,
         from: from,
         name: null,
@@ -135,13 +136,13 @@ class TogglerProvider extends NotifierProviderImpl<Toggler, bool> {
   }
 
   @override
-  NotifierProviderElement<Toggler, bool> createElement() {
-    return _TogglerProviderElement(this);
+  NotifierProviderElement<TogglerFalse, bool> createElement() {
+    return _TogglerFalseProviderElement(this);
   }
 
   @override
   bool operator ==(Object other) {
-    return other is TogglerProvider && other.key == key;
+    return other is TogglerFalseProvider && other.key == key;
   }
 
   @override
@@ -155,17 +156,159 @@ class TogglerProvider extends NotifierProviderImpl<Toggler, bool> {
 
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
-mixin TogglerRef on NotifierProviderRef<bool> {
+mixin TogglerFalseRef on NotifierProviderRef<bool> {
   /// The parameter `key` of this provider.
   String get key;
 }
 
-class _TogglerProviderElement extends NotifierProviderElement<Toggler, bool>
-    with TogglerRef {
-  _TogglerProviderElement(super.provider);
+class _TogglerFalseProviderElement
+    extends NotifierProviderElement<TogglerFalse, bool> with TogglerFalseRef {
+  _TogglerFalseProviderElement(super.provider);
 
   @override
-  String get key => (origin as TogglerProvider).key;
+  String get key => (origin as TogglerFalseProvider).key;
+}
+
+String _$togglerTrueHash() => r'9dc8cb29f2b00e5ca152be6ba72fe428bba2d868';
+
+abstract class _$TogglerTrue extends BuildlessNotifier<bool> {
+  late final String key;
+
+  bool build(
+    String key,
+  );
+}
+
+/// See also [TogglerTrue].
+@ProviderFor(TogglerTrue)
+const togglerTrueProvider = TogglerTrueFamily();
+
+/// See also [TogglerTrue].
+class TogglerTrueFamily extends Family<bool> {
+  /// See also [TogglerTrue].
+  const TogglerTrueFamily();
+
+  /// See also [TogglerTrue].
+  TogglerTrueProvider call(
+    String key,
+  ) {
+    return TogglerTrueProvider(
+      key,
+    );
+  }
+
+  @override
+  TogglerTrueProvider getProviderOverride(
+    covariant TogglerTrueProvider provider,
+  ) {
+    return call(
+      provider.key,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'togglerTrueProvider';
+}
+
+/// See also [TogglerTrue].
+class TogglerTrueProvider extends NotifierProviderImpl<TogglerTrue, bool> {
+  /// See also [TogglerTrue].
+  TogglerTrueProvider(
+    String key,
+  ) : this._internal(
+          () => TogglerTrue()..key = key,
+          from: togglerTrueProvider,
+          name: r'togglerTrueProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$togglerTrueHash,
+          dependencies: TogglerTrueFamily._dependencies,
+          allTransitiveDependencies:
+              TogglerTrueFamily._allTransitiveDependencies,
+          key: key,
+        );
+
+  TogglerTrueProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.key,
+  }) : super.internal();
+
+  final String key;
+
+  @override
+  bool runNotifierBuild(
+    covariant TogglerTrue notifier,
+  ) {
+    return notifier.build(
+      key,
+    );
+  }
+
+  @override
+  Override overrideWith(TogglerTrue Function() create) {
+    return ProviderOverride(
+      origin: this,
+      override: TogglerTrueProvider._internal(
+        () => create()..key = key,
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        key: key,
+      ),
+    );
+  }
+
+  @override
+  NotifierProviderElement<TogglerTrue, bool> createElement() {
+    return _TogglerTrueProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is TogglerTrueProvider && other.key == key;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, key.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+mixin TogglerTrueRef on NotifierProviderRef<bool> {
+  /// The parameter `key` of this provider.
+  String get key;
+}
+
+class _TogglerTrueProviderElement
+    extends NotifierProviderElement<TogglerTrue, bool> with TogglerTrueRef {
+  _TogglerTrueProviderElement(super.provider);
+
+  @override
+  String get key => (origin as TogglerTrueProvider).key;
 }
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
