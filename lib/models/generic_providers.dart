6c2361e8af4cb48edd7f544ecdd8fac939d62dfe
Thomas Schüßler <834347+vindolin@users.noreply.github.com>
2024-12-04 10:42:34 +0100

    WIP

--- lib/models/generic_providers.dart
+++ lib/models/generic_providers.dart
@@ -3,9 +3,18 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'generic_providers.g.dart';
 
 @Riverpod(keepAlive: true)
-class Toggler extends _$Toggler {
+class TogglerFalse extends _$TogglerFalse {
   @override
   bool build(String key) => false;
+
+  void toggle() => state = !state;
+  void set(bool value) => state = value;
+}
+
+@Riverpod(keepAlive: true)
+class TogglerTrue extends _$TogglerTrue {
+  @override
+  bool build(String key) => true;
 
   void toggle() => state = !state;
   void set(bool value) => state = value;
