975473045233f52af9decd1835a404c1adaec9d8
Thomas Schüßler <834347+vindolin@users.noreply.github.com>
2024-12-06 09:45:59 +0100

    WIP

--- lib/widgets/upload_widget.dart
+++ lib/widgets/upload_widget.dart
@@ -1,72 +1,71 @@
 import 'dart:async';
 import 'dart:math';
 
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
-
-import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:dio/dio.dart';
 import 'package:file_picker/file_picker.dart';
 
 import '/models/client_name.dart';
 import '/models/client_provider.dart';
 import '/utils/logger.dart';
 
-part 'upload_widget.g.dart';
+// part 'upload_widget.g.dart';
 
-@riverpod
-class UploadProgress extends _$UploadProgress {
-  @override
-  double build() {
-    return 0.0;
-  }
+// @riverpod
+// class UploadProgress extends _$UploadProgress {
+//   @override
+//   double build() {
+//     return 0.0;
+//   }
 
-  void set(double value) {
-    state = value;
-  }
-}
+//   void set(double value) {
+//     state = value;
+//   }
+// }
 
-@riverpod
-class UploadProgressStream extends _$UploadProgressStream {
-  final StreamController<double> _controller = StreamController<double>();
+// @riverpod
+// class UploadProgressStream extends _$UploadProgressStream {
+//   final StreamController<double> _controller = StreamController<double>();
 
-  @override
-  Stream<double> build() {
-    ref.onDispose(() {
-      _controller.close();
-    });
+//   @override
+//   Stream<double> build() {
+//     ref.onDispose(() {
+//       _controller.close();
+//     });
 
-    return _controller.stream;
-  }
+//     return _controller.stream;
+//   }
 
-  void set(double value) {
-    _controller.add(value);
-  }
-}
+//   void set(double value) {
+//     _controller.add(value);
+//   }
+// }
 
-@riverpod
-class UploadSpeed extends _$UploadSpeed {
-  @override
-  int build() {
-    return 0;
-  }
+// @riverpod
+// class UploadSpeed extends _$UploadSpeed {
+//   @override
+//   int build() {
+//     return 0;
+//   }
 
-  void set(int value) {
-    state = value;
-  }
-}
+//   void set(int value) {
+//     state = value;
+//   }
+// }
 
-@riverpod
-class FileSelectorOpen extends _$FileSelectorOpen {
-  @override
-  bool build() {
-    return false;
-  }
+// @riverpod
+// class FileSelectorOpen extends _$FileSelectorOpen {
+//   @override
+//   bool build() {
+//     return false;
+//   }
 
-  void set(bool value) {
-    state = value;
-  }
-}
+//   void set(bool value) {
+//     state = value;
+//   }
+// }
 
 String formatBytes(int bytes) {
   if (bytes <= 0) return '0 B/s';
@@ -76,73 +75,164 @@ String formatBytes(int bytes) {
   return '${size.toStringAsFixed(2)} ${suffixes[i]}';
 }
 
-class UploadButton extends ConsumerWidget {
+class UploadButton extends ConsumerStatefulWidget {
   final DuktiClient client;
   const UploadButton({super.key, required this.client});
 
-  void _uploadFile(WidgetRef ref) async {
-    final uploadProgressNotifier = ref.read(uploadProgressProvider.notifier);
-    final uploadSpeedNotifier = ref.read(uploadSpeedProvider.notifier);
-    final fileSelectorOpen = ref.read(fileSelectorOpenProvider.notifier);
+  @override
+  ConsumerState<UploadButton> createState() => _UploadButtonState();
+}
 
-    fileSelectorOpen.set(true);
-    final result = await FilePicker.platform.pickFiles();
-    fileSelectorOpen.set(false);
+FutureOr<String?> pickFile() async {
+  final result = await FilePicker.platform.pickFiles();
+  if (result != null && result.files.single.path != null) {
+    final filePath = result.files.single.path!;
+    logger.i('Uploading file: $filePath');
+    return filePath;
+  }
+  return null;
+}
 
-    if (result != null && result.files.single.path != null) {
-      final filePath = result.files.single.path!;
-      final fileName = result.files.single.name;
+void uploadFile(
+  String filePath,
+  DuktiClient client,
+  Function(double) updateProgress,
+  Function(int) updateSpeed,
+) async {
+  // fileSelectorOpen.set(true);
+  // setState(() => fileSelectorOpen2 = true);
 
-      final dio = Dio();
+  // final result = await FilePicker.platform.pickFiles();
 
-      final formData = FormData.fromMap({
-        'file': await MultipartFile.fromFile(filePath, filename: fileName),
-        'clientId': clientId,
+  // // fileSelectorOpen.set(false);
+  // setState(() => fileSelectorOpen2 = false);
+
+  // final filePath = '/data/user/0/com.atomar.dukti/cache/file_picker/1733349332034/youtube.apk';
+
+  logger.i('Uploading file: $filePath');
+
+  final dio = Dio();
+
+  final formData = FormData.fromMap({
+    'file': await MultipartFile.fromFile(filePath),
+    'clientId': clientId,
+  });
+
+  final address = 'http://${client.ip}:${client.port}/upload';
+  logger.i('Uploading to: $address');
+
+  final startTime = DateTime.now();
+
+  try {
+    await dio.post(
+      address,
+      data: formData,
+      onSendProgress: (int sent, int total) {
+        // dependency injection of stream controller?
+
+        final currentTime = DateTime.now();
+        final elapsedTime = currentTime.difference(startTime).inSeconds;
+        if (elapsedTime > 0) {
+          final speed = sent / elapsedTime; // bytes per second
+
+          // uploadSpeedNotifier.set(speed.toInt());
+          updateSpeed(speed.toInt());
+        }
+        final progress = sent / total;
+
+        // uploadProgressNotifier.set(progress);
+        updateProgress(progress);
+
+        logger.t('Upload progress dio: $progress');
+      },
+    );
+    Future.delayed(const Duration(seconds: 1), () {
+      updateProgress(0.0);
+    });
+
+    logger.i('Upload complete');
+  } catch (e) {
+    logger.i('Upload failed: $e');
+  }
+}
+
+class _UploadButtonState extends ConsumerState<UploadButton> {
+  bool fileSelectorOpen = false;
+  double uploadProgress = 0.0;
+  int uploadSpeed = 0;
+
+  void _uploadFile(String filePath, DuktiClient client) async {
+    // fileSelectorOpen.set(true);
+    // setState(() => fileSelectorOpen2 = true);
+
+    // final result = await FilePicker.platform.pickFiles();
+
+    // // fileSelectorOpen.set(false);
+    // setState(() => fileSelectorOpen2 = false);
+
+    // final filePath = '/data/user/0/com.atomar.dukti/cache/file_picker/1733349332034/youtube.apk';
+
+    logger.i('Uploading file: $filePath');
+
+    final dio = Dio();
+
+    final formData = FormData.fromMap({
+      'file': await MultipartFile.fromFile(filePath),
+      'clientId': clientId,
+    });
+
+    final address = 'http://${client.ip}:${client.port}/upload';
+
+    logger.i('Uploading to: $address');
+
+    final startTime = DateTime.now();
+
+    try {
+      await dio.post(
+        address,
+        data: formData,
+        onSendProgress: (int sent, int total) {
+          // dependency injection of stream controller?
+
+          final currentTime = DateTime.now();
+          final elapsedTime = currentTime.difference(startTime).inSeconds;
+          if (elapsedTime > 0) {
+            final speed = sent / elapsedTime; // bytes per second
+
+            // uploadSpeedNotifier.set(speed.toInt());
+            if (mounted) setState(() => uploadSpeed = speed.toInt());
+          }
+          final progress = sent / total;
+
+          // uploadProgressNotifier.set(progress);
+          if (mounted) setState(() => uploadProgress = progress);
+
+          logger.t('Upload progress dio: $progress');
+        },
+      );
+      Future.delayed(const Duration(seconds: 1), () {
+        if (mounted) setState(() => uploadProgress = 0.0);
+        if (mounted) setState(() => uploadSpeed = 0);
       });
 
-      final address = 'http://${client.ip}:${client.port}/upload';
-
-      final startTime = DateTime.now();
-
-      try {
-        await dio.post(
-          address,
-          data: formData,
-          onSendProgress: (int sent, int total) {
-            final currentTime = DateTime.now();
-            final elapsedTime = currentTime.difference(startTime).inSeconds;
-            if (elapsedTime > 0) {
-              final speed = sent / elapsedTime; // bytes per second
-              uploadSpeedNotifier.set(speed.toInt());
-            }
-            final progress = sent / total;
-            uploadProgressNotifier.set(progress);
-            logger.t('Upload progress dio: $progress');
-          },
-        );
-        Future.delayed(const Duration(seconds: 1), () {
-          uploadProgressNotifier.set(0.0);
-          uploadSpeedNotifier.set(0);
-        });
-
-        logger.i('Upload complete');
-      } catch (e) {
-        logger.i('Upload failed: $e');
-      }
+      logger.i('Upload complete');
+    } catch (e) {
+      logger.i('Upload failed: $e');
     }
   }
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final uploadProgress = ref.watch(uploadProgressProvider);
-    final uploadSpeed = ref.watch(uploadSpeedProvider);
-    final fileSelectorOpen = ref.watch(fileSelectorOpenProvider);
-
+  Widget build(BuildContext context) {
     logger.e('Upload progress build: $uploadProgress');
 
     return uploadProgress == 0.0
         ? FilledButton(
-            onPressed: fileSelectorOpen ? null : () => _uploadFile(ref),
+            onPressed: () async {
+              final filePath = await pickFile();
+              if (filePath != null) {
+                _uploadFile(filePath, widget.client);
+              }
+            },
             style: ButtonStyle(
               backgroundColor:
                   WidgetStateProperty.all(fileSelectorOpen ? Colors.grey : Theme.of(context).colorScheme.primary),
