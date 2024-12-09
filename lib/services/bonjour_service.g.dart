201fe75f01b8b352ff6c794627c30dfe25ede9e0
Thomas Schüßler <834347+vindolin@users.noreply.github.com>
2024-12-09 11:03:54 +0100

    same

--- lib/services/bonjour_service.g.dart
+++ lib/services/bonjour_service.g.dart
@@ -26,5 +26,23 @@ final eventsProvider =
 // ignore: unused_element
 typedef EventsRef
     = AutoDisposeStreamProviderRef<List<bonsoir.BonsoirDiscoveryEvent>>;
+String _$isBroadcastingHash() => r'625ff3f3a6391511027c1a5e25d08d171b3b936c';
+
+/// provider for the broadcast status
+///
+/// Copied from [IsBroadcasting].
+@ProviderFor(IsBroadcasting)
+final isBroadcastingProvider =
+    AutoDisposeNotifierProvider<IsBroadcasting, bool>.internal(
+  IsBroadcasting.new,
+  name: r'isBroadcastingProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$isBroadcastingHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$IsBroadcasting = AutoDisposeNotifier<bool>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
