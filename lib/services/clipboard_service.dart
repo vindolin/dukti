f9c8d3b9a2c5c04dae82e7e144cde94fb71ea2c7
Thomas Schüßler <834347+vindolin@users.noreply.github.com>
2024-12-09 11:34:16 +0100

    convert clipboard provider from stream to notifier

--- lib/services/clipboard_service.dart
+++ lib/services/clipboard_service.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -12,19 +10,13 @@ part 'clipboard_service.g.dart';
 
 @riverpod
 class ClipboardService extends _$ClipboardService {
-  final StreamController<String> _controller = StreamController<String>();
-
   @override
-  Stream<String> build() {
-    ref.onDispose(() {
-      _controller.close();
-    });
-
-    return _controller.stream;
+  String? build() {
+    return null;
   }
 
-  void set(String text) {
-    _controller.add(text);
+  void set(String? text) {
+    state = text;
   }
 }
 
