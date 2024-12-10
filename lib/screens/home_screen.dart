f32787178f0b2a7293ff22802da258c7ce756461
Thomas Schüßler <834347+vindolin@users.noreply.github.com>
2024-12-10 10:12:42 +0100

    refactor: replace FToast with ScaffoldMessenger for clipboard notifications

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 import '/models/client_provider.dart';
 import '/services/bonjour_service.dart';
@@ -12,72 +11,29 @@ import 'client_list/widgets/client_list_widget.dart';
 
 import '../utils/logger.dart';
 
-class DuktiHome extends ConsumerStatefulWidget {
+class DuktiHome extends ConsumerWidget {
   final String title;
   const DuktiHome({super.key, required this.title});
 
   @override
-  ConsumerState<DuktiHome> createState() => _DuktiHomeState();
-}
-
-class _DuktiHomeState extends ConsumerState<DuktiHome> {
-  FToast? fToast;
-  bool isBroadcasting = true;
-
-  //TODO use ScaffoldMessenger instead of FToast
-  @override
-  initState() {
-    super.initState();
-
-    // initialize the toast
-    fToast = FToast();
-    fToast?.init(context);
-  }
-
-  _showToast(String message) {
-    final toast = Container(
-      decoration: BoxDecoration(
-        color: Colors.grey[300],
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.black26,
-            blurRadius: 10.0,
-            spreadRadius: 2.0,
-            offset: Offset(5.0, 5.0),
-          ),
-        ],
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(
-          message,
-          style: const TextStyle(color: Colors.black),
-        ),
-      ),
-    );
-
-    // if we don't use a future here, the toast will throw an error
-    Future(
-      () => fToast?.showToast(
-        child: toast,
-        gravity: ToastGravity.BOTTOM,
-        toastDuration: const Duration(seconds: 2),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     logger.t('Building DuktiHome');
     final clipboard = ref.watch(clipboardServiceProvider);
     final clients = ref.watch(duktiClientsProvider);
     // final useDarkTheme = ref.watch(togglerTrueProvider('darkTheme'));
 
     if (clipboard != null) {
+      Future(() {
+        if (context.mounted) {
+          return ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text('Clipboard received:\n${clipboard.substring(0, 100)}', overflow: TextOverflow.ellipsis),
+          ));
+        }
+      });
       Clipboard.setData(ClipboardData(text: clipboard));
-      _showToast(clipboard.toString());
-      Future.delayed(const Duration(seconds: 2), () {
+
+      // Clear the clipboard
+      Future(() {
         ref.read(clipboardServiceProvider.notifier).set(null);
       });
     }
